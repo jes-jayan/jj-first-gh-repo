d93ec7a326fc41a3cdb3e0d8ad67a6c0cdba218c
jes-jayan <jaya0068@algonquinlive.com>
2025-01-14 15:00:16 -0500

    Updated index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
+//TASK 1
+
+let a = 3;
+let b = 5;
+let c;
+
+
+let output1 = `
+let a = 3;
+let b = 5;
+let c;
+
+	a + b = ${a + b};
+	a - b = ${a - b},
+	a * b = ${a * b},
+	a / b = ${a / b},
+	a % b = ${a % b},
+	a += b = ${a += b};
+	a -= b = ${a -= b};
+	a *= b = ${a *= b};
+	a /= b = ${a /= b};
+	a %= b = ${a %= b};
+	a == b = ${a == b};
+	a != b = ${a != b};
+	a > b = ${a > b};
+	a < b = ${a < b};
+	!a && !c = ${!a && !c};
+	!a || !c  = ${!a || !c };
+`
+console.log(output1);
+
+
+//TASK 2
+let first_name = 'Jesma';
+let last_name = 'Jayan';
+let email = 'jaya0068@algonquinlive.com'
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+
+console.log(output);
 
